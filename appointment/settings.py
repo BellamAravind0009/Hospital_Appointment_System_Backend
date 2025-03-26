50c30f51bb60b7fb92f94d4a5c7c0b242eb21025
BellamAravind0009 <bellamaravind24@gmail.com>
2025-03-26 22:31:18 +0530

    Update settings.py

--- appointment/settings.py
+++ appointment/settings.py
@@ -69,6 +69,8 @@ WSGI_APPLICATION = "appointment.wsgi.application"
 # Database: Uses Render PostgreSQL
 DATABASES = {
     "default": {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         "ENGINE": "django.db.backends.postgresql",
         "NAME": env("DATABASE_NAME"),
         "USER": env("DATABASE_USER"),
